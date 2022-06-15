88733e0369bbf06afef493ead236e11db7010c50
atiburrahman09 <atiburrahman09@gmail.com>
2022-06-15 14:59:27 +0600

    bug fixed

--- ARMMordanizerService/ARMMordanizerService/ConnectionDB/ConnectionDb.cs
+++ ARMMordanizerService/ARMMordanizerService/ConnectionDB/ConnectionDb.cs
@@ -15,10 +15,10 @@ namespace ARMMordanizerService
         public ConnectionDb()
         {
             con=new SqlConnection(_connectionString);
-            if(con.State == System.Data.ConnectionState.Open)
-            {
-                con.Close();
-            }
+            //if(con.State == System.Data.ConnectionState.Open)
+            //{
+            //    con.Close();
+            //}
         }
     }
 }
